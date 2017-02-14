7a45b3c8895f6adb22b13e625fc2b7febc061529
blueness927 <blueness927@gmail.com>
2017-02-14 10:30:47 +0800

    登錄帳號改成UserName

--- prjPetAdoption/Controllers/AccountController.cs
+++ prjPetAdoption/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace prjPetAdoption.Controllers
 
             // 這不會計算為帳戶鎖定的登入失敗
             // 若要啟用密碼失敗來觸發帳戶鎖定，請變更為 shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
