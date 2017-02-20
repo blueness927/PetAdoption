da8d48d33b85b9c3da479cb98db462cef1fb7fcc
blueness927 <blueness927@gmail.com>
2017-02-21 02:07:41 +0800

    半夜兩點才要睡

--- prjPetAdoption/Scripts/index.js
+++ prjPetAdoption/Scripts/index.js
@@ -86,38 +86,38 @@ function carouselNormalization() {
     }
 }
 
-//cat animate
-$(window).mousemove(function(evt){  
-  var x = evt.pageX; 
-  var y = evt.pageY; 
-  //console.log( x + "," + y);
+////cat animate
+//$(window).mousemove(function(evt){  
+//  var x = evt.pageX; 
+//  var y = evt.pageY; 
+//  //console.log( x + "," + y);
   
-  $("#cross").css("left",x+"px");
-  $("#cross").css("top",y+"px");
+//  $("#cross").css("left",x+"px");
+//  $("#cross").css("top",y+"px");
   
-  var catplace = $("#cat_blue").offset().left+$("#cat_blue").width()/2;
-  if (Math.abs(x-catplace)<80){
-    $("#cat_blue").css("bottom","0px");
-  }else{
-    $("#cat_blue").css("bottom","-50px");
-  }
+//  var catplace = $("#cat_blue").offset().left+$("#cat_blue").width()/2;
+//  if (Math.abs(x-catplace)<80){
+//    $("#cat_blue").css("bottom","0px");
+//  }else{
+//    $("#cat_blue").css("bottom","-50px");
+//  }
   
-  var catplace = $("#cat_yellow").offset().left+$("#cat_yellow").width()/2;
-  if (Math.abs(x-catplace)<80){
-    $("#cat_yellow").css("bottom","0px");
-  }else{
-    $("#cat_yellow").css("bottom","-50px");
-  }
+//  var catplace = $("#cat_yellow").offset().left+$("#cat_yellow").width()/2;
+//  if (Math.abs(x-catplace)<80){
+//    $("#cat_yellow").css("bottom","0px");
+//  }else{
+//    $("#cat_yellow").css("bottom","-50px");
+//  }
   
-  var catplace = $("#cat_grey").offset().left+$("#cat_grey").width()/2;
-  if (Math.abs(x-catplace)<80){
-    $("#cat_grey").css("bottom","0px");
-  }else{
-    $("#cat_grey").css("bottom","-50px");
-  }
+//  var catplace = $("#cat_grey").offset().left+$("#cat_grey").width()/2;
+//  if (Math.abs(x-catplace)<80){
+//    $("#cat_grey").css("bottom","0px");
+//  }else{
+//    $("#cat_grey").css("bottom","-50px");
+//  }
   
   
-});
+//});
 
 $(document).ready(function(){
   //Check to see if the window is top if not then display button
